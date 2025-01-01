955ebb9b19e55da000bd04c75a8d4713e6663c93
sarfrazryenpsd <mdsarfraz.ilanos1915@gmail.com>
2025-01-02 00:11:03 +0530

    JDBC Config for railway

--- src/main/kotlin/com/example/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/example/repository/DatabaseFactory.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
@@ -17,17 +18,24 @@ object DatabaseFactory {
 
         transaction {
             SchemaUtils.create(UserTable)
-            SchemaUtils.create(NoteTable)
+            SchemaUtils.create(NoteTable )
         }
     }
 
-    fun hikari(): HikariDataSource{
+    private fun hikari(): HikariDataSource{
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER")
-        config.jdbcUrl = System.getenv("DATABASE_URL")
+       // config.jdbcUrl = System.getenv("DATABASE_URL")
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val username = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        config.jdbcUrl = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
+
         config.validate()
 
         return HikariDataSource(config)
